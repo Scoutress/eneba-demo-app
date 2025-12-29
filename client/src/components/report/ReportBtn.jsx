ab8886415035167a14058c051f20b7f7dd87deb3
Scoutress <142579732+Scoutress@users.noreply.github.com>
2025-12-29 21:31:55 +0200

    Created basic report btn and more hardcoded games

---
+++ client/src/components/report/ReportBtn.jsx
@@ -0,0 +1,9 @@
+import styles from "./ReportBtn.module.scss";
+
+export default function ReportBtn() {
+  return (
+    <div>
+      <button className={styles.btn}>Report a problem</button>
+    </div>
+  );
+}
--- client/src/navigation/Root.jsx
+++ client/src/navigation/Root.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Header from "../components/header/Header.jsx";
+import ReportBtn from "../components/report/ReportBtn.jsx";
 
 const Root = () => {
   return (
     <>
       <Header />
       <Outlet />
+      <ReportBtn />
     </>
   );
 };
