cda4f96cd32c6b3c652dd8eef18a39933afe2e4d
Scoutress <142579732+Scoutress@users.noreply.github.com>
2025-12-29 16:20:04 +0200

    Added logo to the header

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,9 +1,15 @@
+import styles from "./Header.module.scss";
+import logo from "../../assets/logo.svg";
+
 export default function Header() {
   return (
-    <header className="header">
-      <div className="header__logo">basic component</div>
+    <header className={styles.header}>
+      <a href="/" className={styles.logo}>
+        <img src={logo} alt="App logo" />
+      </a>
 
-      <nav className="header__nav">
+      {/* Temp.section */}
+      <nav className={styles.nav}>
         <a href="/">Home</a>
         <a href="/about">About</a>
         <a href="/contact">Contact</a>
--- client/src/pages/listPage/List.jsx
+++ client/src/pages/listPage/List.jsx
@@ -1,9 +1,7 @@
 const List = () => {
   return (
     <div>
-      <Component1 /> {/* will be created */}
-      <Component2 /> {/* will be created */}
-      <Component3 /> {/* will be created */}
+      {/* <Component1 /> */}
       will be created
     </div>
   );
