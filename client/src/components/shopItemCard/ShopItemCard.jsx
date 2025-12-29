4e2f0ab75708c021b5fb790db7129bfa24460e57
Scoutress <142579732+Scoutress@users.noreply.github.com>
2025-12-29 21:15:00 +0200

    Created simple shop cards with hardcoded games

--- client/src/components/shopItemCard/ShopItemCard.jsx
+++ client/src/components/shopItemCard/ShopItemCard.jsx
@@ -1,5 +1,49 @@
 import styles from "./ShopItemCard.module.scss";
 
-export default function ShopItemCard({ title }) {
-  return <div className={styles.card}>{title}</div>;
+export default function ShopItemCard({
+  photo,
+  platform,
+  title,
+  region,
+  fullprice,
+  discount,
+  price,
+  cashback,
+  wishlisted,
+}) {
+  return (
+    <>
+      <div className={styles.card}>
+        {/* Wishlist */}
+        <button className={styles.wishlist}>♡</button>
+
+        {/* Image */}
+        <div className={styles.photo}>
+          <img src={photo} alt={title} />
+        </div>
+
+        {/* Content */}
+        <div className={styles.content}>
+          <div className={styles.platform}>{platform}</div>
+          <div className={styles.title}>{title}</div>
+          <div className={styles.region}>{region}</div>
+
+          <div className={styles.old_prices}>
+            <span>From </span>
+            <span className={styles.fullprice}>{fullprice}</span>
+            <span className={styles.discount}>{discount}</span>
+          </div>
+
+          <div className={styles.price}>{price}</div>
+          <div className={styles.cashback}>{cashback}</div>
+          <div className={styles.wishlisted}>{wishlisted}</div>
+
+          <div className={styles.actions}>
+            <button className={styles.primary}>Add to cart</button>
+            <button className={styles.secondary}>Explore options</button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
--- client/src/pages/listPage/List.jsx
+++ client/src/pages/listPage/List.jsx
@@ -1,17 +1,86 @@
 import ItemsGrid from "../../components/itemsGrid/ItemsGrid.jsx";
 import ShopItemCard from "../../components/shopItemCard/ShopItemCard.jsx";
 
+import photo1 from "../../assets/games/split_fiction_universal.jpg";
+import photo2 from "../../assets/games/split_fiction_nintendo.jpg";
+
+const items = [
+  {
+    id: 1,
+    photo: photo1,
+    platform: "platform",
+    title: "title",
+    region: "region",
+    fullprice: "fullprice",
+    price: "price",
+    cashback: "cashback",
+    wishlisted: "wishlisted",
+  },
+  {
+    id: 2,
+    photo: photo2,
+    platform: "platform",
+    title: "title",
+    region: "region",
+    fullprice: "fullprice",
+    price: "price",
+    cashback: "cashback",
+    wishlisted: "wishlisted",
+  },
+  {
+    id: 3,
+    photo: photo2,
+    platform: "platform",
+    title: "title",
+    region: "region",
+    fullprice: "fullprice",
+    price: "price",
+    cashback: "cashback",
+    wishlisted: "wishlisted",
+  },
+  {
+    id: 4,
+    photo: photo2,
+    platform: "platform",
+    title: "title",
+    region: "region",
+    fullprice: "fullprice",
+    price: "price",
+    cashback: "cashback",
+    wishlisted: "wishlisted",
+  },
+  {
+    id: 5,
+    photo: photo2,
+    platform: "platform",
+    title: "title",
+    region: "region",
+    fullprice: "fullprice",
+    price: "price",
+    cashback: "cashback",
+    wishlisted: "wishlisted",
+  },
+];
+
 const List = () => {
   return (
     <>
       <div>Results found: 12</div>
       <div>
         <ItemsGrid>
-          <ShopItemCard title="Item1" />
-          <ShopItemCard title="Item2" />
-          <ShopItemCard title="Item3" />
-          <ShopItemCard title="Item4" />
-          <ShopItemCard title="Item5" />
+          {items.map((item) => (
+            <ShopItemCard
+              key={item.id}
+              photo={item.photo}
+              platform={item.platform}
+              title={item.title}
+              region={item.region}
+              fullprice={item.fullprice}
+              price={item.price}
+              cashback={item.cashback}
+              wishlisted={item.wishlisted}
+            />
+          ))}
         </ItemsGrid>
       </div>
     </>
