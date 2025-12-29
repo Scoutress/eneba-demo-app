77537c42abddbebec953e4642ad6c220d0b5db2f
Scoutress <142579732+Scoutress@users.noreply.github.com>
2025-12-29 17:25:13 +0200

    Changed error page

--- client/src/pages/errorPage/ErrorPage.jsx
+++ client/src/pages/errorPage/ErrorPage.jsx
@@ -1,8 +1,19 @@
+import styles from "./ErrorPage.module.scss";
+
 const ErrorPage = () => {
   return (
-    <div>
-      <h1>404</h1>
-      <h2>Will be created</h2>
+    <div className={styles.container}>
+      <h1 className={styles.title}>404</h1>
+      <p className={styles.message}>
+        Oops! The page you are looking for does not exist.
+      </p>
+      <p className={styles.description}>
+        It seems that the link you clicked may be broken or the page has been
+        removed. Please check the URL or return to the homepage.
+      </p>
+      <a href="/list" className={styles.homeLink}>
+        Back to the main page
+      </a>
     </div>
   );
 };
