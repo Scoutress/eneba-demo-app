a8ab94d7b9749e364a7fbc815c7413fb6338167b
Scoutress <142579732+Scoutress@users.noreply.github.com>
2025-12-30 17:33:12 +0200

    Added functionality for sold items
    Also added more hardcoded dummy data

--- client/src/components/shopItemCard/ShopItemCard.jsx
+++ client/src/components/shopItemCard/ShopItemCard.jsx
@@ -12,10 +12,59 @@ export default function ShopItemCard({
   discount,
   currentPrice,
   priceInfo,
+  hasCashback,
   cashback,
   wishlisted,
-  // available,
+  available,
 }) {
+  if (!available) {
+    return (
+      <article className={`${styles.card} ${styles.sold} `}>
+        <div className={styles.media}>
+          <img className={styles.photo} src={photo} alt={title} />
+
+          <button
+            type="button"
+            className={styles.wishlistBtn}
+            aria-label="Add to wishlist"
+          >
+            ♡
+          </button>
+        </div>
+
+        <div className={styles.panel}>
+          <div className={styles.panelBg} />
+
+          <div className={styles.platformBar}>
+            <img className={styles.platformIcon} src={platformIcon} />
+            <span className={styles.platformText}>{platform}</span>
+          </div>
+
+          <div className={styles.content}>
+            <h3 className={styles.title}>{title}</h3>
+
+            <div className={styles.regionRow}>
+              <span className={regionOk ? styles.regionOk : styles.regionBad}>
+                {regionLabel}
+              </span>
+            </div>
+
+            <div className={styles.baseRow}>
+              <span className={styles.from}>X</span>
+              <span className={styles.from}>Sold out</span>
+            </div>
+
+            <div className={styles.wishlistCount}>
+              <span className={styles.heart}>♡</span>
+              <span>{wishlisted}</span>
+            </div>
+          </div>
+        </div>
+        <div className={styles.soldOverlay} />
+      </article>
+    );
+  }
+
   return (
     <article className={styles.card}>
       <div className={styles.media}>
@@ -32,10 +81,13 @@ export default function ShopItemCard({
 
       <div className={styles.panel}>
         <div className={styles.panelBg} />
-        <div className={styles.cashbackBadge}>
-          <span className={styles.badgeIcon}>↺</span>
-          <span>CASHBACK</span>
-        </div>
+
+        {hasCashback && (
+          <div className={styles.cashbackBadge}>
+            <span className={styles.badgeIcon}>↺</span>
+            <span>CASHBACK</span>
+          </div>
+        )}
 
         <div className={styles.platformBar}>
           <img className={styles.platformIcon} src={platformIcon} />
